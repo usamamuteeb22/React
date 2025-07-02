598180fa8c91ee572593c7fca0eaa00fa53037f1
usamamuteeb22 <usamaweb246@gmail.com>
2025-07-02 17:13:15 +0500

    prop types app uplaod

---
+++ prop-types/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from './Student.jsx'
+
+function App(){
+  return(
+    <>
+    <Student name='Usama' age={24} isStudent={true}/>
+    <Student name='Ahmad' age={34} isStudent={true}/>
+    <Student name='Ali' age={3} isStudent={false}/>
+    <Student/>
+    </>
+  )
+}
+export default App
