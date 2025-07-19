c0416a1c5b7020caf8edd62798d72c513e06ac19
usamamuteeb22 <usamaweb246@gmail.com>
2025-07-19 19:49:37 +0500

    color-picker app upload

---
+++ color-picker/src/App.jsx
@@ -0,0 +1,8 @@
+import ColorPicker from "./colorPicker.jsx";
+
+function App(){
+  return(
+    <ColorPicker/>
+  )
+}
+export default App
---
+++ color-picker/src/colorPicker.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+function ColorPicker(){
+    const [color, selectColor] = useState('black')
+    return(
+        <div className='container'>
+            <h1>Color Picker</h1>
+            <div className='color-cont' style={{backgroundColor:color}}>
+                <h2>Selected Color : {color}</h2>
+            </div>
+            <input type="color" value={color} onChange={(e)=>selectColor(e.target.value)} className='color' />
+        </div>
+    )
+}
+export default ColorPicker
